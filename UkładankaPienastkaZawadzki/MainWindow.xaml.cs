27d20b48345f617bf89aaa5cafa0bb348955ef0e
4234572 <daniil.gavrilov@teb.edu.pl>
2026-02-23 12:12:06 +0100

    Dodaj obsługę przesuwania kafelków w układance
    Dodano obsługę kliknięć dla wszystkich przycisków w siatce oraz logikę przesuwania kafelków do pustego miejsca, jeśli sąsiadują z nim w wierszu lub kolumnie. Dodano metodę wykrywającą wolne pole w siatce. Ustawiono x:Name="MainGrid" dla głównej siatki w XAML. Drobna zmiana w using (dodano System.Data.Common).

--- UkładankaPienastkaZawadzki/MainWindow.xaml.cs
+++ UkładankaPienastkaZawadzki/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data.Common;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,9 +22,64 @@ namespace UkładankaPienastkaZawadzki
             InitializeComponent();
         }
 
+        private (int Row, int Col) WhereIsEmptySpace()
+        {
+            int rows = MainGrid.RowDefinitions.Count;
+            int cols = MainGrid.ColumnDefinitions.Count;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    bool occupied = false;
+                    foreach (Button button in MainGrid.Children)
+                    {
+                        if (Grid.GetRow(button) == r && Grid.GetColumn(button) == c)
+                        {
+                            occupied = true;
+                            break;
+                        }
+                    }
+
+                    if (!occupied)
+                    {
+                        return (r, c);
+                    }
+                }
+            }
+
+            return (-1, -1);
+        }
+
         private void CheckForFreePlace_Click(object sender, RoutedEventArgs e)
         {
+            if (sender is not Button button)
+                return;
 
+            int row = Grid.GetRow(button);
+            int col = Grid.GetColumn(button);
+
+            var emptySpace  = WhereIsEmptySpace();
+
+            if (emptySpace.Row == row)
+            {
+                int emptyToBtnDiff = System.Math.Abs(emptySpace.Col - col);
+                if (emptyToBtnDiff == 1)
+                {
+                    Grid.SetColumn(button, emptySpace.Col);
+                }
+                return;
+            }
+
+            if (emptySpace.Col == col)
+            {
+                int emptyToBtnDiff = System.Math.Abs(emptySpace.Row - row);
+                if (emptyToBtnDiff == 1)
+                {
+                    Grid.SetRow(button, emptySpace.Row);
+                }
+                return;
+            }
         }
     }
 }
